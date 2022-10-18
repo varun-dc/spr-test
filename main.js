7c8b3fdb46892f4b6126e7245798b025f80a74a4
varun-dc <varun@deepchannel.com>
2022-10-18 09:00:53 -0400

    Add main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+function doSomething(a, b, c, d) {
+  console.log(a);
+  console.log(b);
+  console.log(c);
+  console.log(d);
+}
+
+function main() {
+  console.log("Hello World");
+  doSomething("a", "b", "c", "d");
+}
