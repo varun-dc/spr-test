adcfd211c73c60790d93c06947096364ba8c3ef7
varun-dc <varun@deepchannel.com>
2022-10-18 09:39:46 -0400

    feat: add featureA2
    Closes X-2
    
    commit-id:f0ea5d4f

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+function featureA2() {
+  console.log('feature a2');
+}
+
 function featureA1(x, y, z) {
   console.log(x, y, z);
 }
@@ -13,4 +17,5 @@ function main() {
   console.log("Hello World");
   doSomething("a", "b", "c", "d");
   featureA1('x', 'y', 'z');
+  featureA2();
 }
